9a143c9c5ed22e6b21e9d85640eba74381482ff6
Adrian Blanc <adrian@adrianblanc.live>
2022-06-24 21:26:17 -0500

    Added templates & main login page

--- nebulei/apps/core/urls.py
+++ nebulei/apps/core/urls.py
@@ -3,5 +3,4 @@ from . import views
 
 urlpatterns = [
     path('', views.core_home, name='core-home'),
-    path('about/', views.core_about, name='core-about'),
 ]
--- nebulei/apps/core/views.py
+++ nebulei/apps/core/views.py
@@ -4,5 +4,3 @@ from django.shortcuts import render
 def core_home(request):
     return render(request, 'core/views/core_home.html')
 
-def core_about(request):
-    return render(request, 'core/views/core_about.html')
