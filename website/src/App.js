54a26202725b0c5dcf8d86aba578f5f34826e27e
Adrian Blanc <adrian@adrianblanc.live>
2022-07-07 14:59:37 -0500

    boilerplate auth react

---
+++ website/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Auth from './Auth/App'
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Auth></Auth>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ website/src/Auth/App.js
@@ -0,0 +1,37 @@
+import React from "react"
+import { Container } from "react-bootstrap"
+import { AuthProvider } from "../contexts/AuthContext"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+
+import Signup from "./components/Signup"
+import Dashboard from "./components/Dashboard"
+import Login from "./components/Login"
+import PrivateRoute from "./components/PrivateRoute"
+import ForgotPassword from "./components/UpdateProfile"
+import UpdateProfile from "./components/UpdateProfile"
+
+
+function App() {
+  return (
+    <Container
+      className="d-flex align-items-center justify-content-center"
+      style={{ minHeight: "100vh" }}
+    >
+      <div className="w-100" style={{ maxWidth: "400px" }}>
+        <Router>
+          <AuthProvider>
+            <Switch>
+              <PrivateRoute exact path="/" component={Dashboard} />
+              <PrivateRoute path="/update-profile" component={UpdateProfile} />
+              <Route path="/signup" component={Signup} />
+              <Route path="/login" component={Login} />
+              <Route path="/forgot-password" component={ForgotPassword} />
+            </Switch>
+          </AuthProvider>
+        </Router>
+      </div>
+    </Container>
+  )
+}
+
+export default App
