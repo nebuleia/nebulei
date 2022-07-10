a56b2c75f03fbd2fcbc179849b3abcc1895087b0
Adrian Blanc <adrian@adrianblanc.live>
2022-07-10 14:40:17 -0500

    deletion of auth

--- website/src/App.js
+++ website/src/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
-import Auth from './Auth/App'
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Auth></Auth>
+        
       </div>
     );
   }
